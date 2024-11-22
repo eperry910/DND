dca01f4a8245b50010c9a2b496b65bdabf78ad8d
eperry910 <eperry910@yahoo.com>
2024-11-22 15:51:46 -0500

    Added in baseline MongoDB Access

--- Controllers/CharacterSheetController.cs
+++ Controllers/CharacterSheetController.cs
@@ -2,34 +2,38 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using MongoDB.Driver;
 using DND.Models;
-namespace DND.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-public class CharacterSheetController : ControllerBase
+namespace DND.Controllers
 {
-    private static List<CharacterSheet> characterSheets = new List<CharacterSheet>();
-
-    private readonly ILogger<CharacterSheetController> _logger;
-
-    public CharacterSheetController(ILogger<CharacterSheetController> logger)
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterSheetController : ControllerBase
     {
-        _logger = logger;
-    }
+        private readonly IMongoCollection<CharacterSheet> _characterSheets;
+        private readonly ILogger<CharacterSheetController> _logger;
 
-    [HttpPost(Name = "New Character")]
-    public IActionResult Post([FromBody] CharacterSheet characterSheet)
-    {
-        if(characterSheet == null){
-            return BadRequest("Character sheet cannot be null");
+        public CharacterSheetController(IMongoCollection<CharacterSheet> characterSheets, ILogger<CharacterSheetController> logger)
+        {
+            _characterSheets = characterSheets;
+            _logger = logger;
         }
-        characterSheets.Add(characterSheet);
-        return Ok(characterSheet);
-        
-    }
-    [HttpGet(Name = "Classes")]
-    public IActionResult Get()
-    {
-        return Ok(characterSheets);
+
+        [HttpPost(Name = "New Character")]
+        public IActionResult Post([FromBody] CharacterSheet characterSheet)
+        {
+            if (characterSheet == null)
+            {
+                return BadRequest("Character sheet cannot be null");
+            }
+            _characterSheets.InsertOne(characterSheet);
+            return Ok(characterSheet);
+        }
+
+        [HttpGet(Name = "Get All Characters")]
+        public IActionResult Get()
+        {
+            var characterSheets = _characterSheets.Find(sheet => true).ToList();
+            return Ok(characterSheets);
+        }
     }
 }
